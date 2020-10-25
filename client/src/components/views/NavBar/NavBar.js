4a1a9d955d3365db1c87394710ff0302c223c691
IngEoGeoBuk <59249899+IngEoGeoBuk@users.noreply.github.com>
2020-10-26 01:07:41 +0900

    Update NavBar.js

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import LeftMenu from './Sections/LeftMenu';
 import RightMenu from './Sections/RightMenu';
-import { Drawer, Button, Icon } from 'antd';
+import { Drawer, Button } from 'antd';
 import './Sections/Navbar.css';
+import { AlignRightOutlined } from '@ant-design/icons';
 const Logo = require('../../../assets/images/sportsicon2.png');
 
 function NavBar() {
@@ -33,7 +34,7 @@ function NavBar() {
           type="primary"
           onClick={showDrawer}
         >
-          <Icon type="align-right" />
+          <AlignRightOutlined />
         </Button>
         <Drawer
           title="Basic Drawer"
